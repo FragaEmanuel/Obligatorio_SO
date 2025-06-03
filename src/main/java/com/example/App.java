6f57166364fa1c72e448d01da79c749c5d085669
FragaEmanuel <emanuel.fraga@correo.ucu.edu.uy>
2025-06-03 18:35:07 -0300

    Me habia dejado un prueba

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,48 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        pruebapriorbaja prueba = new pruebapriorbaja();
-        pruebaprioralta prueba2 = new pruebaprioralta();
-        pruebapriorbaja prueba3 = new pruebapriorbaja();
-        pruebaprioralta prueba4 = new pruebaprioralta();
-        pruebapriorbaja prueba5 = new pruebapriorbaja();
-        pruebaprioralta prueba6 = new pruebaprioralta();
-        pruebapriorbaja prueba7 = new pruebapriorbaja();
-        pruebaprioralta prueba8 = new pruebaprioralta();
-        prueba2.start();
-        prueba.start();
-        prueba3.start();
-        prueba4.start();
-        prueba5.start();
-        prueba6.start();
-        prueba7.start();
-        prueba8.start();
 
-    }
-
-}
-
-
-class pruebapriorbaja extends Thread{
-
-    public pruebapriorbaja() {
-        this.setPriority(5);
-    }
-
-    public void run() {
-        System.out.println("Prioridad baja");
-    
     }
 }
-
-class pruebaprioralta extends Thread{
-
-    public pruebaprioralta() {
-        this.setPriority(10);
-    }
-
-    public void run() {
-            System.out.println("Prioridad alta");
-    }
-}
-
